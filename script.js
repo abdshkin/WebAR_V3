400f9642625ac52fe06b1488202457452158f395
abdshkin <abdshkin@gmail.com>
2025-03-07 02:12:37 +0500

    update for anim

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
-function playSpeech() {
-    let audio = document.getElementById("voice");
+let audio = document.getElementById("voice");
+
+function playSpeech () {
     audio.play();
 }
 
 function stopSpeech() {
-    let audio = document.getElementById("voice");
     audio.pause();
     audio.currentTime = 0;
 }
 
-function toggleAnimation() {
-    let model = document.querySelector("#model");
+let model = document.querySelector("#model");
 
+AFRAME.registerComponent('animation-mixer', {
+    init: function () {
+        this.el.addEventListener('model-loaded', () => {
+            this.mixer = new THREE.AnimationMixer(this.el.getObject3D('mesh'));
+            this.mixer.clipAction(this.el.getObject3D('mesh').animations[0]).play();
+        });
+    },
+    tick: function (time, deltaTime) {
+        if (this.mixer) this.mixer.update(deltaTime / 1000);
+    }
+});
+
+function toggleAnimation() {
     if (model.hasAttribute("animation-mixer")) {
-        model.removeAttribute("animation-mixer"); // Останавливаем анимацию
+        model.removeAttribute("animation-mixer"); // Stopping the animation
     } else {
-        model.setAttribute("animation-mixer", "clip: Mirzhakip.002Action; loop: repeat"); // Запускаем анимацию
+        model.setAttribute("animation-mixer", "clip: Mirzhakip.002Action; loop: repeat"); // Start animation
     }
 }
 
